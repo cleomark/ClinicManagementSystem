7bd91b2b7c3a2c8f6d910af8828b91d7b51708c3
ryukkn <126628758+ryukkn@users.noreply.github.com>
2023-09-19 17:13:49 +0800

    ken2

--- user/usergsjhs/adddentalmessagegsjhs.php
+++ user/usergsjhs/adddentalmessagegsjhs.php
@@ -47,7 +47,25 @@
 
     <!-- App CSS -->  
     <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
-	<link rel="stylesheet" href="assets/style.css">
+	  <link rel="stylesheet" href="assets/style.css">
+
+    <link type="text/css" href="css/bootstrap.min.css" />
+        <link type="text/css" href="css/bootstrap-timepicker.min.css" />
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap-timepicker.min.js"></script>
+    <!-- Include Bootstrap CDN -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <!-- Include Moment.js CDN -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+
+    <!-- Include Bootstrap DateTimePicker CDN -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+    <script src ="calendar.js"></script>
 
     
 
@@ -317,11 +335,11 @@ body{
 }
 .wrapper{
   width: 550px;
-  height:520px;
+  height:490px;
   background: #fff;`
   border-radius: 10px;
   box-shadow: 0 15px 40px rgba(0,0,0,0.12);
-  margin:0px 120px;
+  margin:0% 25%;
 }
 .wrapper header{
   display: flex;
@@ -357,6 +375,7 @@ header .current-date{
 }
 .calendar{
   padding: 20px;
+  height:350px;
 }
 .calendar ul{
   display: flex;
@@ -414,15 +433,16 @@ header .current-date{
 /* Modal Content */
 .modal{
    
-    top:50%;
-    left:4%;
+    top:30%;
+    left:6%;
 }
 .modal-content {
   background-color: #fefefe;
   margin: auto;
   padding: 20px;
   border: 1px solid #888;
-  width: 450px;
+  right:2%;
+  width: 400px;
 }
 
 /* The Close Button */
@@ -439,6 +459,112 @@ header .current-date{
   text-decoration: none;
   cursor: pointer;
 }
+
+/* TIme  */
+/* (A) WRAPPER */
+#tp-wrap * {
+	font-family: arial, sans-serif;
+	box-sizing: border-box;
+  }
+  #tp-wrap *::selection {
+	background: transparent;
+  }
+  #tp-wrap {
+	width: 100vw; height: 100vh;
+	position: fixed; top: 0; left: 0; z-index: 999;
+	background: rgba(0, 0, 0, 0.7);
+	opacity: 0; visibility: hidden;
+	transition: opacity 0.3s;
+  }
+  #tp-wrap.show {
+	opacity: 1; visibility: visible;
+  }
+  
+  /* (B) BOX */
+  #tp-box {
+	position: absolute;
+	top: 55%; left: 54%;
+	transform: translate(-50%, -50%);
+	width: 320px;
+	display: flex; flex-wrap: wrap;
+	border: 1px solid #000;
+	background: #fff;
+  }
+  
+  /* (C) HR/MIN/AM/PM */
+  .tp-cell {
+	width: 33.3%; padding: 0 15px;
+	text-align: center;
+  }
+  .tp-up, .tp-down {
+	padding: 10px 0;
+	color: #e01717;
+	font-size: 32px; font-weight: 1000;
+	cursor: pointer;
+  }
+  .tp-val {
+	width: 100%; padding: 10px 0;
+	text-align: center; font-size: 22px;
+	background: #fff;
+  }
+  
+  /* (D) CLOSE & SET BUTTON */
+  #tp-close, #tp-set {
+	width: 50%; padding: 15px 0; border: 0;
+	font-size: 18px; font-weight: 700;
+	color: #fff; cursor: pointer;
+  }
+  #tp-close { background: #951a1a; }
+  #tp-set { background: #1339a7; }
+  
+  /* (E) 24-HOUR MODIFY */
+  #tp-wrap.tp-24 #tp-ap { display: none; }
+  #tp-wrap.tp-24 #tp-hr, #tp-wrap.tp-24 #tp-min { width: 50%; }
+  
+  /* (X) CSS DOES NOT MATTER - FOR COSMETICS */
+
+  /* WIDGET */
+  .widget-wrap {
+	width: 500px;
+	border-radius: 20px;
+  }
+  .widget-wrap label, .widget-wrap input {
+	display: block;
+	padding: 10px;
+	width: 100%;
+  }
+  #demoA{
+    background:blue;
+    border-color:blue;
+    padding: 5px;
+    border-radius:10px;
+    width:150px;
+    justify:space-evenly;
+    margin-left:40%;
+    cursor:pointer;
+    text-align: center;
+    color:white;
+  }
+  #demoA:hover{
+    border-color:#00b2ca;
+    background:#00b2ca;
+    padding: 5px;
+    border-radius:10px;
+    width:150px;
+    justify:space-evenly;
+    margin-left:40%;
+    cursor:pointer;
+    text-align: center;
+    color:black;
+  }
+
+  input::placeholder{
+  color: #fff;
+}
+
+
+
+
 </style>
 <?php
     class Calendar {
@@ -688,17 +814,22 @@ header .current-date{
           <li>Sat</li>
         </ul>
         <ul class="days" id="myBtn"></ul>
-      </div>
     </div>
+
+
+    <div class="widget-wrap">
+            <div id="hash"></div>
+            <input type="text" id="demoA" placeholder="SELECT TIME"/>  
+        </div>
+      </div>
+        
       <!-- The Modal -->
       <div id="myModal" class="modal">
 
         <!-- Modal content -->
         <div class="modal-content">
           <span class="close">&times; </span>
-          <p>THIS FUCKING SHIT IS SAMPLE BITCHES</p>
-          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit tenetur totam vel rem perspiciatis, dolore ad, quam qui dicta odio, et repellendus molestiae non officiis. Magnam et laudantium exercitationem earum.
-          </p>
+          
         </div>
       </div>
     
@@ -764,31 +895,203 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
 });
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+//time
+
+var tp = {
+  // (A) INIT - GENERATE TIME PICKER HTML
+  hwrap : null, // entire html time picker
+  hhr : null,   // html hour value
+  hmin : null,  // html min value
+  hap : null,   // html am/pm value
+  init : () => {
+    // (A1) ADD TIME PICKER TO BODY
+    tp.hwrap = document.createElement("div");
+    tp.hwrap.id = "tp-wrap";
+    document.body.appendChild(tp.hwrap);
+
+    // (A2) TIME PICKER INNER HTML
+    tp.hwrap.innerHTML =
+    `<div id="tp-box">
+      <div class="tp-cell" id="tp-hr">
+        <div class="tp-up">&#65087;</div> <div class="tp-val">0</div> <div class="tp-down">&#65088;</div>
+      </div>
+      <div class="tp-cell" id="tp-min">
+        <div class="tp-up">&#65087;</div> <div class="tp-val">0</div> <div class="tp-down">&#65088;</div>
+      </div>
+      <div class="tp-cell" id="tp-ap">
+        <div class="tp-up">&#65087;</div> <div class="tp-val">AM</div> <div class="tp-down">&#65088;</div>
+      </div>
+      <button id="tp-close" onclick="tp.hwrap.classList.remove('show')">Close</button>
+      <button id="tp-set" onclick="tp.set()">Set</button>
+    </div>`;
+
+    // (A3) GET VALUE ELEMENTS + SET CLICK ACTIONS
+    for (let segment of ["hr", "min", "ap"]) {
+      let up = tp.hwrap.querySelector(`#tp-${segment} .tp-up`),
+          down = tp.hwrap.querySelector(`#tp-${segment} .tp-down`);
+      tp["h"+segment] = tp.hwrap.querySelector(`#tp-${segment} .tp-val`);
+
+      if (segment=="ap") {
+        up.onclick = () => tp.spin(true, segment);
+        down.onclick = () => tp.spin(true, segment);
+      } else {
+        up.onmousedown = () => tp.spin(true, segment);
+        down.onmousedown = () => tp.spin(false, segment);
+        up.onmouseup = () => tp.spin(null);
+        down.onmouseup = () => tp.spin(null);
+        up.onmouseleave = () => tp.spin(null);
+        down.onmouseleave = () => tp.spin(null);
+      }
+    }
+  },
+
+  // (B) SPIN HOUR/MIN/AM/PM
+  //  direction : true (up), false (down), null (stop)
+  //  segment : "hr", "min", "ap" (am/pm)
+  timer : null, // for "continous" time spin
+  minhr : 1,    // min spin limit for hour
+  maxhr : 12,   // max spin limit for hour
+  minmin : 0,   // min spin limit for minute
+  maxmin : 0,  // max spin limit for minute
+  spin : (direction, segment) => {
+    // (B1) CLEAR TIMER
+    if (direction==null) { if (tp.timer!=null) {
+      clearTimeout(tp.timer);
+      tp.timer = null;
+    }}
+
+    // (B2) SPIN FOR AM/PM
+    else if (segment=="ap") { tp.hap.innerHTML = tp.hap.innerHTML=="AM" ? "PM" : "AM"; }
+
+    // (B3) SPIN FOR HR/MIN
+    else {
+      // (B3-1) INCREMENT/DECREMENT
+      let next = +tp["h"+segment].innerHTML;
+      next = direction ? next+1 : next-1;
+
+      // (B3-2) MIN/MAX
+      if (segment=="hr") {
+        if (next > tp.maxhr) { next = tp.maxhr; }
+        if (next < tp.minhr) { next = tp.minhr; }
+      } else {
+        if (next > tp.maxmin) { next = tp.maxmin; }
+        if (next < tp.minmin) { next = tp.minmin; }
+      }
+
+      // (B3-3) SET VALUE
+      if (next<10) { next = "0"+next; }
+      tp["h"+segment].innerHTML = next;
+
+      // (B3-4) KEEP ROLLING - LOWER TIMEOUT WILL SPIN FASTER
+      tp.timer = setTimeout(() => tp.spin(direction, segment), 100);
+    }
+  },
+
+  // (C) ATTACH TIME PICKER TO HTML FIELD
+  //  target : html field to attach to
+  //  24 : 24 hours time? default false.
+  //  after : optional, function to run after selecting time
+  attach : instance => {
+    // (C1) READONLY FIELD + NO AUTOCOMPLETE
+    // IMPORTANT, PREVENTS ON-SCREEN KEYBOARD
+    instance.target.readOnly = true;
+    instance.target.setAttribute("autocomplete", "off");
+
+    // (C2) DEFAULT 12 HOURS MODE
+    if (instance["24"]==undefined) { instance["24"] = false; }
+
+    // (C3) CLICK TO OPEN TIME PICKER
+    instance.target.addEventListener("click", () => tp.show(instance));
+  },
+
+  // (D) SHOW TIME PICKER
+  setfield : null, // set selected time to this html field
+  set24 : false,   // 24 hours mode? default false.
+  setafter : null, // run this after selecting time
+  show : instance => {
+    // (D1) INIT FIELDS TO SET + OPTIONS
+    tp.setfield = instance.target;
+    tp.setafter = instance.after;
+    tp.set24 = instance["12"];
+    tp.minhr = tp.set24 ? 1 : 8 ;
+    tp.maxhr = tp.set24 ? 3 : 11 ;
+
+    // (D2) READ CURRENT VALUE
+    let val = tp.setfield.value;
+    if (val=="") {
+      tp.hhr.innerHTML = "0"+tp.minhr;
+      tp.hmin.innerHTML = "0"+tp.minmin;
+      tp.hap.innerHTML = "AM";
+    } else {
+      tp.hhr.innerHTML = val.substring(0, 2);
+      if (tp.set24) {
+        tp.hmin.innerHTML = val.substring(2, 4);
+      } else {
+        tp.hmin.innerHTML = val.substring(3, 5);
+        tp.hap.innerHTML = val.substring(6, 8);
+      }
+    }
+
+    // (D3) SHOW TIME PICKER
+    if (tp.set24) { tp.hwrap.classList.add("tp-24"); }
+    else { tp.hwrap.classList.remove("tp-24"); }
+    tp.hwrap.classList.add("show");
+  },
+
+  // (E) SET TIME
+  set : () => {
+    // (E1) TIME TO FIELD
+    if (tp.set24) {
+      tp.setfield.value = tp.hhr.innerHTML + tp.hmin.innerHTML;
+    } else {
+      tp.setfield.value = tp.hhr.innerHTML + ":" + tp.hmin.innerHTML + " " + tp.hap.innerHTML;
+    }
+    tp.hwrap.classList.remove("show");
+
+    // (E2) RUN AFTER, IF SET
+    if (tp.setafter) { tp.setafter(tp.setfield.value); }
   }
-}
+};
+document.addEventListener("DOMContentLoaded", tp.init);
+
+// (F) ATTACH ON PAGE LOAD
+document.addEventListener("DOMContentLoaded", () => {
+  tp.init();
+  tp.attach({
+    target: document.getElementById("demoA")
+  });
+  // tp.attach({
+  //   target: document.getElementById("demoB"),
+  //   "24" : true, // 24 hours time
+  //   after : time => alert(time) // run function after select
+  // });
+});
 
 </script>
 
